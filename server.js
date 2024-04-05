236c1026acf43b52d2f0744c1dc2228eb5837782
saavadas <slava_0629@bk.ru>
2024-04-05 20:31:49 +0500

    1

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.listen(3500, () => console.log(`Server running on port 3500`));
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
