72f87c5bdda0df609102e10971387573cee985e6
saavadas <slava_0629@bk.ru>
2024-04-06 21:43:54 +0500

    1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+const axios = require("axios");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
@@ -18,12 +18,40 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/bitrix", (req, res) => {
+app.post("/bitrix", async (req, res) => {
   console.log(req.body);
-  console.log(req.query);
-  console.log(req.params);
-  return res.json("Success");
+  try {
+    const response = await axios.post(
+      // "https://ihouse.bitrix24.ru/rest/222/8sdi9a5ddmjokdvr/crm.deal.get.json",
+      "https://b24-qmabnl.bitrix24.ru/rest/1/0wz3j6inen5i7fyi/crm.deal.get.json",
+      // { id: req.body.data.FIELDS.ID }
+      { id: req.body.id }
+    );
+    console.log(response.data.result);
+    res.status(200).send("Data sent successfully");
+  } catch (error) {
+    console.error("Error sending data:", error.message);
+    res.status(500).send("Error sending data");
+  }
+  // return res.json("Success");
 });
+
+app.post("/bitrix/create", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://b24-qmabnl.bitrix24.ru/rest/1/w0o94m3l9loivssw/crm.deal.add.json",
+      // { id: req.body.data.FIELDS.ID }
+      { TITLE: req.body.title, OPPORTUNITY: req.body.price }
+    );
+    // console.log(response.data.result);
+    res.status(200).send("Data sent successfully");
+  } catch (error) {
+    console.error("Error sending data:", error.message);
+    res.status(500).send("Error sending data");
+  }
+  // return res.json("Success");
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
